dfca9882c41d0c9effde8fd1500f19a76a30d1d0
guojunhao <guojunhao@qq.com>
2023-06-20 15:35:50 +0800

    创建项目

---
+++ src/main/java/com/qfedu/springbootcrm/SpringBootCrmApplication.java
@@ -0,0 +1,13 @@
+package com.qfedu.springbootcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrmApplication.class, args);
+    }
+
+}
